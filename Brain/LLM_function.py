1e73503b7d63997e80654d1018abf51c65b18135
ha1772007 <ha1772007@gmail.com>
2024-06-29 03:25:06 +0000

    Groq Organinzed

--- Brain/LLM_function.py
+++ Brain/LLM_function.py
@@ -1,48 +1,9 @@
-import json
-from groq import Groq
-class GroqMyClient():
-    def Groq_ask(messages,model,key):
-        client = Groq(
-        api_key=key,
-        )
-        try:
-            chat_completion = client.chat.completions.create(
-                messages=messages,
-                model=model,
-            )  
-            return(groq_response_senitizer(chat_completion))
-        except Exception as e:
-            return e
-def check_groq_api(key):
-    return True
-def groq_response_senitizer(response):
-    final = {}
-    try:
-        answer = []
-        for choice in response.choices:
-            answer.append(choice.message.content)
-        final['output'] = answer
-    except:
-        print('Unable to parse Response choices \n',response)
-        return False
-    try:
-        final['Other'] = {}
-    except:
-        print('Unexpected Error')
-        return False
-    try:
-        final['Other']['id'] = response.id
-    except:
-        print('Unable to get ID')
-    return final
 
+from Providers.mygroq import *
 def MyGroq(messages,key,model='llama-3-70b'):
     
     if(key):
-        if(check_groq_api(key)):
-            pass
-        else:
-            return 'Not Working API key'
+        pass
     else:
         return 'Provide a Valid API key'
     print(type(messages))
@@ -52,12 +13,12 @@ def MyGroq(messages,key,model='llama-3-70b'):
     except:
         return 'Type: Invalid Message Formate \nType of Message is Not JSON santizable'
     if(model == 'llama-3-70b'):
-        return GroqMyClient.Groq_ask(messages,'llama3-70b-8192',key)
+        return GroqMyClient.Groq_ask(messages=messages,model='llama3-70b-8192',key=key)
     elif(model == 'llama-3-8b'):
-        return GroqMyClient.Groq_ask(messages,'llama3-8b-8192',key)
+        return GroqMyClient.Groq_ask(messages=messages,model='llama3-8b-8192',key=key)
 
     elif(model == 'mixtral-8x7b'):
-        return GroqMyClient.Groq_ask(messages,'mixtral-8x7b-32768',key)
+        return GroqMyClient.Groq_ask(messages=messages,model='mixtral-8x7b-32768',key=key)
     else:
         return 'Now a known Model'
     return 'Under Maintainance'
