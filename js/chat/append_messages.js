3b5848cbf5fb23237012427f4b945cae2af4b9fc
ha1772007 <ha1772007@gmail.com>
2024-08-12 16:00:50 +0530

    Incomplete Formula Error Maths Improved Improved Coad printing

--- js/chat/append_messages.js
+++ js/chat/append_messages.js
@@ -1,13 +1,17 @@
-function append_user(message,full){
-    let template = `<div class="message-content human-message h-auto w-[99%] flex p-2" content='${Stable_encoder(full)}'>
+function append_user(message, full) {
+  let template = `<div class="message-content human-message h-auto w-[99%] flex p-2" content='${Stable_encoder(full)}'>
     <div class="w-[90%] bg-dark-2 ml-[10%] p-2 rounded-md">${message}</div>
   </div>`
   $('#message_container').append(template)
+  MathJax.typesetPromise()
 }
-function append_ai(message,content){
-    let template = `<div class="message-content AI-message h-auto w-[99%] flex p-2" content="${Stable_encoder(content)}">
-    <div class="w-[100%] bg-dark-2 rounded-l-md p-2 min-h-[20vh]">${message}</div>
+function append_ai(message, content) {
+  var converter = new showdown.Converter();
+  let m = converter.makeHtml(content);
+  let template = `<div class="message-content AI-message h-auto w-[99%] flex p-2" content="${Stable_encoder(content)}">
+    <div class="w-[100%] bg-dark-2 rounded-l-md p-2 min-h-[20vh]">${m}</div>
     
   </div>`
   $('#message_container').append(template)
+  MathJax.typesetPromise()
 }
